a948c8dfa85837e5d0f6353148b1cef72bb89868
GeorgijP <122865026+GeorgijP@users.noreply.github.com>
2023-01-20 15:54:11 +0400

    Update str_func.py
    Добавлена функция up_first_letter. И описание

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def up_letters(line):
     """Принимает на вход строку и возвращает её, со всеми заглавными буквами"""
     return line.upper()
+
+
+def up_first_letter(line):
+    """Делает заглавными первые буквы каждого слова в строке"""
+    return line.title()
+    
